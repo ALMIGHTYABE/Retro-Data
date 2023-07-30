6e9d71db99fbbff76ec3794bed126725f178b3d2
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2023-07-30 22:26:05 +0530

    Update pair_data.py

--- pair_data.py
+++ pair_data.py
@@ -127,7 +127,7 @@ try:
     
     # Today and 2 Day Ago
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(2)
+    twodayago = todayDate - timedelta(4)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
