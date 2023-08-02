83ef6ebeebef2947893c58c18b64f3b4041ac0df
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2023-08-02 18:04:46 +0530

    Issue fixed

--- revenue_data.py
+++ revenue_data.py
@@ -42,12 +42,10 @@ try:
     bribe_df = pd.read_csv(bribe_data)
     fee_df = pd.read_csv(fee_data)
     pair_df = pd.read_csv(pair_data)
-    ids_df = pd.read_csv(id_data)
-    ids_df["epoch"] = epoch-1
 
     # Get Epoch Timestamp
     todayDate = datetime.utcnow()
-    if todayDate.isoweekday() == 4 and todayDate.hour>4:
+    if todayDate.isoweekday() == 4 and todayDate.hour > 4:
         nextThursday = todayDate + relativedelta(weekday=TH(2))
         my_time = datetime.min.time()
         my_datetime = datetime.combine(nextThursday, my_time)
@@ -64,6 +62,10 @@ try:
     epoch_data = pd.read_csv(epoch_csv)
     epoch = epoch_data[epoch_data["timestamp"] == timestamp]["epoch"].values[0]
 
+    # Read IDS Data
+    ids_df = pd.read_csv(id_data)
+    ids_df["epoch"] = epoch - 1
+
     # Pull Prices
     response = requests.get(price_api)
     RETRO_price = jmespath.search("data[?name == 'RETRO'].price", response.json())[0]
@@ -74,36 +76,61 @@ try:
 
     voteweight = []
     for bribe in ids_df["gauge.bribe"]:
-        print(ids_df[ids_df['gauge.bribe']==bribe]['symbol'].values[0])
+        print(ids_df[ids_df["gauge.bribe"] == bribe]["symbol"].values[0])
         if bribe == "0x0000000000000000000000000000000000000000":
             voteweight.append(0)
         else:
             contract_instance = w3.eth.contract(address=bribe, abi=bribe_abi)
-            voteweight.append(contract_instance.functions.totalSupplyAt(timestamp).call() / 1000000000000000000)
+            voteweight.append(
+                contract_instance.functions.totalSupplyAt(timestamp).call()
+                / 1000000000000000000
+            )
     ids_df["voteweight"] = voteweight
 
-
     # Data Wrangling
-    pair_df.columns = ['id', 'date', 'tvlUSD', 'volumeUSD', 'volumeToken0', 'volumeToken1', 'token0Price', 'token1Price', 'feesUSD', '__typename', 'name_pool', 'underlyingPool', 'type', 'epoch']
-    epoch_wise_pair_fees = pair_df.groupby(["epoch", "name_pool"], as_index=False)["feesUSD"].sum()
-    epoch_wise_fees = fee_df.groupby(["epoch", "name_pool"], as_index=False)["fee_amount"].sum()
-    epoch_wise_bribes = bribe_df.groupby(["epoch", "name_pool"], as_index=False)["bribe_amount"].sum()
-    df = pd.merge(epoch_wise_fees, epoch_wise_pair_fees, on=["epoch", "name_pool"], how="outer")
+    pair_df.columns = [
+        "id",
+        "date",
+        "tvlUSD",
+        "volumeUSD",
+        "volumeToken0",
+        "volumeToken1",
+        "token0Price",
+        "token1Price",
+        "feesUSD",
+        "__typename",
+        "name_pool",
+        "underlyingPool",
+        "type",
+        "epoch",
+    ]
+    epoch_wise_pair_fees = pair_df.groupby(["epoch", "name_pool"], as_index=False)[
+        "feesUSD"
+    ].sum()
+    epoch_wise_fees = fee_df.groupby(["epoch", "name_pool"], as_index=False)[
+        "fee_amount"
+    ].sum()
+    epoch_wise_bribes = bribe_df.groupby(["epoch", "name_pool"], as_index=False)[
+        "bribe_amount"
+    ].sum()
+    df = pd.merge(
+        epoch_wise_fees, epoch_wise_pair_fees, on=["epoch", "name_pool"], how="outer"
+    )
     df = pd.merge(df, epoch_wise_bribes, on=["epoch", "name_pool"], how="outer")
     df.replace(np.nan, 0, inplace=True)
-    df.columns = ['epoch', 'name_pool', 'fee_amount', 'total_feesUSD', 'bribe_amount']
+    df.columns = ["epoch", "name_pool", "fee_amount", "total_feesUSD", "bribe_amount"]
     df["voter_share"] = df["fee_amount"] + df["bribe_amount"]
     df["revenue"] = df["total_feesUSD"] + df["bribe_amount"]
     bribe_df_offset = epoch_wise_bribes.copy(deep=True)
     bribe_df_offset["epoch"] = bribe_df_offset["epoch"] + 1
     bribe_df_offset.columns = ["epoch", "name_pool", "bribe_amount_offset"]
     df = pd.merge(df, bribe_df_offset, on=["epoch", "name_pool"], how="outer")
-    ids_df = ids_df[['symbol', 'epoch', 'voteweight']]
-    ids_df.columns = ['name_pool', 'epoch', 'voteweight']
+    ids_df = ids_df[["symbol", "epoch", "voteweight"]]
+    ids_df.columns = ["name_pool", "epoch", "voteweight"]
     final_df = pd.merge(df, ids_df, on=["epoch", "name_pool"], how="outer")
     final_df["RETRO_price"] = RETRO_price
     final_df["votevalue"] = final_df["voteweight"] * final_df["RETRO_price"]
-    final_df['vote_apr'] = final_df['voter_share']/final_df['votevalue']*100*52
+    final_df["vote_apr"] = final_df["voter_share"] / final_df["votevalue"] * 100 * 52
     final_df.replace(np.nan, 0, inplace=True)
     final_df.replace([np.inf, -np.inf], 0, inplace=True)
     final_df.sort_values(by="epoch", axis=0, ignore_index=True, inplace=True)
@@ -133,4 +160,6 @@ try:
 
     logger.info("Revenue Data Ended")
 except Exception as e:
-    logger.error("Error occurred during Revenue Data process. Error: %s" % e, exc_info=True)
+    logger.error(
+        "Error occurred during Revenue Data process. Error: %s" % e, exc_info=True
+    )
