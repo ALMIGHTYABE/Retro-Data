7c150ecb80454db7d7c1a90a4d6c146ebc842c8c
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2023-08-21 22:26:29 +0530

    Updated partner data
    Dune query implemented

--- partner_data.py
+++ partner_data.py
@@ -10,6 +10,8 @@ import itertools
 import concurrent.futures
 from web3 import Web3
 from web3.middleware import validation
+import time
+from requests import get, post
 
 # Params
 params_path = "params.yaml"
@@ -28,12 +30,20 @@ try:
 
     # Params Data
     id_data = config["files"]["id_data"]
+    price_api = config["api"]["price_api"]
     provider_url = os.environ["RPC"]
     bribe_abi = config["web3"]["bribe_abi"]
     epoch_csv = config["files"]["epoch_data"]
     partner_data = config["files"]["partner_data"]
     revenue_data = config["files"]["revenue_data"]
 
+    # Pull Prices
+    response = get(price_api)
+    pricelist = []
+    for i in response.json()["data"]:
+        pricelist.append([i["name"], i["address"], i["price"], i["decimals"]])
+    price_df = pd.DataFrame(pricelist, columns=["name", "address", "price", "decimals"])
+    
     # Get Epoch Timestamp
     todayDate = datetime.utcnow()
     my_time = datetime.min.time()
@@ -51,7 +61,69 @@ try:
     epoch_data = epoch_data[epoch_data["epoch"]>=0]
 
     partners_df = pd.read_csv(partner_data)
+    partners_df["nft_address"] = partners_df["nft_address"].str.lower()
     revenue_df = pd.read_csv(revenue_data)
+
+    # Read Dune Data and wrangling
+    API_KEY = os.environ["DUNE"]
+    HEADER = {"x-dune-api-key": API_KEY}
+
+    BASE_URL = "https://api.dune.com/api/v1/"
+
+    def make_api_url(module, action, ID):
+        url = BASE_URL + module + "/" + ID + "/" + action
+        return url
+
+    def execute_query(query_id, engine="large"):
+        url = make_api_url("query", "execute", query_id)
+        params = {
+            "performance": engine,
+        }
+        response = post(url, headers=HEADER, params=params)
+        execution_id = response.json()["execution_id"]
+        return execution_id
+
+    def get_query_status(execution_id):
+        url = make_api_url("execution", "status", execution_id)
+        response = get(url, headers=HEADER, timeout=60)
+        return response
+
+    def get_query_results(execution_id):
+        url = make_api_url("execution", "results", execution_id)
+        response = get(url, headers=HEADER)
+        return response
+
+    def check_query_completion(execution_id):
+        while True:
+            time.sleep(5)
+            response = get_query_status(execution_id)
+            if response.json()["state"] == "QUERY_STATE_COMPLETED":
+                break
+        return True
+
+    execution_id = execute_query("2944562", "large")
+
+    if check_query_completion(execution_id) == True:
+        response = get_query_results(execution_id)
+        bribe_df = pd.DataFrame(response.json()["result"]["rows"])
+
+    bribe_df["reward_provider"] = bribe_df["reward_provider"].str.lower()
+    bribe_df = pd.merge(bribe_df, partners_df, left_on="reward_provider", right_on="nft_address")
+    bribe_df = bribe_df.merge(price_df[["name", "address", "price", "decimals"]], left_on="reward_token", right_on="address", how="left")
+    bribe_df.dropna(axis=0, inplace=True)
+    bribe_df.reset_index(drop=True, inplace=True)
+    bribe_df["reward_amount"] = bribe_df["reward_amount"].astype(float)
+    bribe_df["bribe_amount"] = bribe_df["price"] * bribe_df["reward_amount"]
+    bribe_df["decimals"] = bribe_df["decimals"].astype(int)
+
+    bribe_amount = []
+    for dec, amt in zip(bribe_df["decimals"], bribe_df["bribe_amount"]):
+        decimal = "1"
+        decimal = decimal.ljust(dec + 1, "0")
+        bribe_amount.append((amt / int(decimal)))
+
+    bribe_df["bribe_amount"] = bribe_amount
+    bribe_df.drop(labels=['reward_amount', 'reward_provider', 'reward_token', 'name', 'address', 'price', 'decimals'], axis=1, inplace=True)
 
     # Web3 and more pandas
     validation.METHODS_TO_VALIDATE = []
@@ -64,35 +136,37 @@ try:
             voteweight = contract_instance.functions.balanceOfOwnerAt(partner_address, timestamp).call()
             symbol = ids_df.loc[(ids_df["gauge.bribe"] == bribe_ca), ['symbol']].values[0][0]
             if voteweight != 0:
-                vote_data.append({'partner_name': partner_name, 'partner_address': partner_address, 'epoch': current_epoch-1, 'symbol': symbol, 'voteweight': voteweight / 1e18})
+                vote_data.append({'partner_name': partner_name, 'partner_address': partner_address, 'epoch': current_epoch-1, 'symbol': symbol, 'reward_pool': bribe_ca.lower(), 'voteweight': voteweight / 1e18})
         except Exception as e:
             print(f"Error processing {partner_name}: {e}")
 
     vote_data = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
-        for args in itertools.product(partners_df['partner_name'], [timestamp], ids_df['gauge.bribe']):
+        for args in itertools.product(bribe_df['partner_name'].unique(), [timestamp], ids_df['gauge.bribe']):
             futures.append(executor.submit(get_vote_data, *args))
     
     vote_df = pd.DataFrame(vote_data)
     vote_df.sort_values("epoch", ascending=False, inplace=True)
     total_vote = vote_df.groupby(['partner_name', 'epoch'])['voteweight'].transform(lambda g: g.sum())
     vote_df['Vote %'] = vote_df['voteweight']/total_vote * 100
+    vote_df = pd.merge(vote_df, bribe_df[["reward_pool", "bribe_amount"]], on="reward_pool", how="outer")
+    vote_df.replace(np.nan, 0, inplace=True)
 
     revenue_df.rename(columns = {'name_pool':'symbol', 'voteweight':'total_voteweight'}, inplace = True)
     revenue_df_offset = revenue_df.copy(deep=True)
     revenue_df_offset["epoch"] = revenue_df_offset["epoch"] - 1
     revenue_df_offset = revenue_df_offset[revenue_df_offset["epoch"] == current_epoch-1]
     revenue_df_offset = revenue_df_offset[['epoch', 'symbol', 'emissions', 'emissions_value', 'oRETRO_price']]
-    revenue_df = revenue_df[['epoch', 'symbol', 'bribe_amount', 'total_voteweight']]
+    revenue_df = revenue_df[['epoch', 'symbol', 'total_voteweight']]
     revenue_df = revenue_df[revenue_df["epoch"] == current_epoch-1]
 
     vote_df = pd.merge(vote_df, revenue_df, on=["epoch", "symbol"], how="left")
     vote_df = pd.merge(vote_df, revenue_df_offset, on=["epoch", "symbol"], how="left")
+    vote_df.replace(np.nan, 0, inplace=True)
     vote_df['Voting Revenue'] = vote_df['bribe_amount']*vote_df['voteweight']/(vote_df['total_voteweight']+0.001)
     vote_df['Spend'] = vote_df['bribe_amount'] - vote_df['Voting Revenue']
     vote_df['Bribe ROI'] = vote_df['emissions_value']/vote_df['Spend']
-    vote_df.replace(np.nan, 0, inplace=True)
     vote_df.replace(np.inf, 0, inplace=True)
     df_values = vote_df.values.tolist()
     print(vote_df)
